e8bc1c73bd2c0bdb654b32e1a425fe39561e2f3f
Changmin Kim <kcm0411@gmail.com>
2024-11-20 21:42:00 +0900

    feat: (JPQL) 채팅방 초대 리스트 Query 작성

--- src/main/java/com/example/chat/repository/UserRepository.java
+++ src/main/java/com/example/chat/repository/UserRepository.java
@@ -2,11 +2,18 @@ package com.example.chat.repository;
 
 import com.example.chat.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
 
     Optional<User> findByUsername(String username);
+
+    // 채팅방 초대 리스트 (전체인원 - 본인 - 이미초대된사람)
+    @Query("SELECT u FROM User u WHERE u.id != :userId AND u NOT IN (SELECT m FROM ChatRoom r JOIN r.members m WHERE r.id = :chatRoomId)")
+    List<User> findAvailableUsersForChatRoom(@Param("userId") Long userId, @Param("chatRoomId") Long chatRoomId);
 
 }
